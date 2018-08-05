21c3e1acbd93963e4cb5e2779c1c5723780ad906
Abhay <abhayrawat2695@gmail.com>
2018-08-06 02:57:22 +0530

    Custom class fro Trajectory created

--- Agent.py
+++ Agent.py
@@ -8,29 +8,35 @@ class Agent:
     assert defaultPose == False or isinstance(defaultPose, Pose), ERR.TYPE_MISMATCH(defaultPose, Pose)
     self.type           = "AGENT"
     self.id             = id
-    self.pose           = pose
-    self.defaultPose    = defaultPose if defaultPose else Pose()
+    self.defaultPose    = defaultPose if defaultPose else pose
     self.collsionRadius = collisionRadius
     self.lenTrajectory  = lenTrajectory
+    self.reset(pose)
 
   def reset(self, pose=False):
     self.prevActions    = []
-    self.trajectory     = deque(maxlen=self.lenTrajectory)
+    self.trajectory     = Trajectory(maxlen=self.lenTrajectory)
     assert pose == False or isinstance(pose, Pose), ERR.BAD_RESET_POSE(pose)
     self.pose = pose if pose else self.defaultPose
     
   def step(self, action, dt=0.01):
+    action = np.matrix(action)
     if action.any():
       self.pose.updateHolonomic(action, dt)
 
   def updateTrajectory(self):
-    self.trajectory.append(self.pose)
+    self.trajectory.append(self.pose.tolist())
 
   def isUnique(self, agents):
     for i in agents:
       if i.id == self.id:
         return False
     return True
+
+  def distanceFrom(self, x):
+    assert isinstance(x, Pose) or isinstance(x, Agent)
+    x = x.pose if isinstance(x, Agent) else x
+    return np.linalg.norm((self.pose - x)[:-1])
 
   def __str__(self):
     info = "{} {}:\n-Pose: {}\n-Default Pose: {}\n-Collision Radius: {}\n".\
@@ -39,6 +45,29 @@ class Agent:
 
   def __repr__(self):
     return self.type + " {}".format(self.id)
+
+class Trajectory:
+  def __init__(self, maxlen=None, data=[]):
+    self.maxlen = maxlen
+    self.data = data
+  
+  def append(self, item):
+    self.data.append(item)
+    if self.maxlen and len(self.data) > self.maxlen:
+      self.data.pop(0)
+
+  def __getitem__(self, index):
+    return self.data[index]
+
+  def __str__(self):
+    return str(self.data)
+
+  def __repr__(self):
+    return str(self.data)
+
+  def __len__(self):
+    return len(self.data)
+
 
 """Should go in RLAgent Class"""
   # def __init__(self):
