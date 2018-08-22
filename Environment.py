548138b2afbb5bb52f81f22c3ccbc9dc12e6c217
Abhay <abhayrawat2695@gmail.com>
2018-08-22 10:43:11 +0530

    Removed mutable as method arg default

--- Environment.py
+++ Environment.py
@@ -5,7 +5,7 @@ from Agent import Agent
 from Visualize import Visualizer
 
 class PointEnvironment(object):
-  def __init__(self, num_iterations=100, dt=0.01, agents=[], visualise=False, visualiseOptions={}):
+  def __init__(self, num_iterations=100, dt=0.01, agents=None, visualise=False, visualiseOptions={}):
     self.iterations = num_iterations
     self.dt         = dt
     self.agents     = {}
@@ -25,6 +25,7 @@ class PointEnvironment(object):
       self.visual.flush()
 
   def addAgents(self, agents):
+    if agents is None: return
     for i in agents:
       self.addAgent(i)
 
@@ -74,20 +75,3 @@ class PointEnvironment(object):
           print ERR.COLLISION(i.id, j.id)
           return True
     return False
-
-
-env = PointEnvironment(num_iterations=100, visualise=True, visualiseOptions={"speedup":10, "tailLength":5, 'bounds':[-6,6,-6,6]})
-env.addAgent(Agent(5, Pose(0,5, 0)))
-env.addAgent(Agent(1, Pose(0,1, 0)))
-env.addAgent(Agent(0, Pose(0,0, 0)))
-env.addAgent(Agent(2, Pose(0,2, 0)))
-env.addAgent(Agent(3, Pose(0,3, 0)))
-env.addAgent(Agent(4, Pose(0,4, 0)))
-print env.agents
-env.visual.thread.start()
-for i in range(10):
-  env.step({k:[k/10., -1/10.] for k in [5,1,2,3,4,0]})
-env.reset({0:Pose(0,-1), 1:Pose(), 2:Pose(0,1), 3:Pose(1,0), 4:Pose(-1,0), 5:Pose(-2,0)})
-
-# for i in range(100):
-#   env.step({k:np.random.random(2)*np.array([.4, .5])-np.array([0, .25]) for k in [5,1,2,3,4,0]})
